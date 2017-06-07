4ee065555e42ba4e9d46e88becda41d957afe4ff
Tristan Nelson <tristan@iseult.us>
2017-06-07 14:05:17 -0400

    Testing use of context + flatten

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.github.jsonld-java/jsonld-java "0.10.0"]
+                 [cheshire "5.7.1"]]
   :main ^:skip-aot clinvar-submitter.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clinvar_submitter/core.clj
+++ src/clinvar_submitter/core.clj
@@ -1,5 +1,31 @@
 (ns clinvar-submitter.core
+  (:require [cheshire.core :as json]
+            [clojure.java.io :as io]
+            [clojure.pprint :refer [pprint]])
+  (:import [com.github.jsonldjava.core JsonLdProcessor JsonLdOptions]
+           [com.github.jsonldjava.utils JsonUtils])
   (:gen-class))
+
+(def test-allele "data/CA090898.jsonld")
+(def allele-cx "data/allelecx.jsonld")
+(def test-interpretation "data/test_interp_1.dmwg.json")
+(def interpretation-cx "data/short-context.jsonld")
+
+(defn flatten-allele
+  []
+  (let [allele (json/parse-stream (io/reader test-allele))
+        context (json/parse-stream (io/reader allele-cx))
+        opts (JsonLdOptions.)]
+    (-> (JsonLdProcessor/flatten allele context opts) JsonUtils/toPrettyString println)))
+
+(defn flatten-interpretation
+  []
+  (with-open [ir (io/reader test-interpretation)
+              cxr (io/reader interpretation-cx)]
+    (let [i (json/parse-stream ir)
+          cx (json/parse-stream cxr)
+          opts (JsonLdOptions.)]
+      (-> (JsonLdProcessor/flatten i cx opts) JsonUtils/toPrettyString println))))
 
 (defn -main
   "I don't do a whole lot ... yet."
