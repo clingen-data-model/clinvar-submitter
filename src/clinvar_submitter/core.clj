cf6c20a54a87ccfce189639d3fb7a701ff0cd78c
Tristan Nelson <tristan@iseult.us>
2017-07-01 09:35:50 -0400

    Factoring LD interaction code into separate file

--- src/clinvar_submitter/core.clj
+++ src/clinvar_submitter/core.clj
@@ -1,86 +1,6 @@
 (ns clinvar-submitter.core
-  (:require [cheshire.core :as json]
-            [clojure.java.io :as io]
-            [clojure.pprint :refer [pprint]]
-            [clojure.walk :as walk])
-  (:import [com.github.jsonldjava.core JsonLdProcessor JsonLdOptions]
-           [com.github.jsonldjava.utils JsonUtils]
-           [java.util Map List])
+  (:require [clinvar-submitter.ld :as ld :refer [ld-> prop=]])
   (:gen-class))
-
-(def test-allele "data/CA090898.jsonld")
-(def allele-cx "data/allelecx.jsonld")
-(def test-interpretation "data/test_interp_1.dmwg.json")
-(def interpretation-cx "data/short-context.jsonld")
-(def frame "data/frame.jsonld")
-
-(defn resolve-id
-  "Return the referent of an identifier if found in symbol table
-  otherwise return a bare string"
-  [t s]
-  (if (instance? Map s) (if-let [v (get s "id")]
-                          (resolve-id t v) s)
-      (if-let [ret (get t s)] ret s)))
-
-(defn ld-get
-  "Get property from a map, if the property is a string matching a key
-  in the symbol table, return the value of that key instead. If the property
-  is a map with a single 'id' property, return either the value of the property
-  or the value of the referent of that property, if it exists in the symbol table"
-  [t loc prop]
-  (cond 
-    (fn? prop) (prop loc)
-    (instance? List loc) (map #(ld-get t % prop) loc)
-    :else (let [v (get loc prop)]
-      (cond 
-        (string? v) (resolve-id t v)
-        (instance? Map v) (if-let [vn (get v "id")] (resolve-id t vn) v)
-        (instance? List v) (map #(resolve-id t %) v)
-        :else v))))
-
-(defn ld->
-  "Take a symbol table, a location, and a set of predicates, 
-  return the set of nodes mapping to a given property"
-  [t loc & preds]
-  (let [res (reduce #(ld-get t %1 %2) loc preds)]
-    ;(println preds res)
-    res))
-
-(defn ld1->
-  "Take the first result of a ld-> path expression"
-  [t loc & preds]
-  (first (ld-> t loc preds)))
-
-(defn prop=
-  "Filter ressults where a specific key (or recursive chain of keys}
-  is equal to value"
-  [t v & ks]
-  (fn [m] (if (instance? List m)
-            (filter (fn [m1] (let [r (apply ld-> t m1 ks)]
-                               (if (instance? List r) (some #(= % v) r) (= r v))))  m)
-            (when (= v (ld-> t m ks)) m))))
-
-(defn convert-jsonld
-  "Convert java structures from jsonld library into clojure structures"
-  [m]
-  (walk/postwalk (fn [n] (cond
-                           (instance? Map n) (into {} n)
-                           (instance? List n) (into [] n)
-                           :else n)) m))
-
-(defn construct-symbol-table
-  [coll]
-  (let [graph (into [] (get coll "@graph"))]
-    (reduce (fn [acc v] (assoc acc (.get v "id") (convert-jsonld v))) {} graph)))
-
-(defn flatten-interpretation
-  []
-  (with-open [ir (io/reader test-interpretation)
-              cxr (io/reader interpretation-cx)]
-    (let [i (json/parse-stream ir)
-          cx (json/parse-stream cxr)
-          opts (JsonLdOptions.)]
-      (-> (JsonLdProcessor/flatten i cx opts) convert-jsonld))))
 
 (defn construct-variant
   "Construct and return one row of variant table, with VariantInterpretation as root"
@@ -89,8 +9,8 @@
 
 (defn construct-variant-table
   "Construct and return variant table"
-  []
-  (let [t (construct-symbol-table (flatten-interpretation))
+  [interp-path context-path]
+  (let [t (ld/read-ld interp-path context-path)
         interps (vals (filter #(= "cg:VariantInterpretation" (-> % val (get "@type"))) t))
         rows (map #(construct-variant t %) interps)]
     rows))
---
+++ src/clinvar_submitter/ld.clj
@@ -0,0 +1,82 @@
+(ns clinvar-submitter.ld
+  (:require [cheshire.core :as json]
+            [clojure.java.io :as io]
+            [clojure.walk :as walk])
+  (:import [com.github.jsonldjava.core JsonLdProcessor JsonLdOptions]
+           [com.github.jsonldjava.utils JsonUtils]
+           [java.util Map List]))
+
+(defn resolve-id
+  "Return the referent of an identifier if found in symbol table
+  otherwise return a bare string"
+  [t s]
+  (if (instance? Map s) (if-let [v (get s "id")]
+                          (resolve-id t v) s)
+      (if-let [ret (get t s)] ret s)))
+
+(defn ld-get
+  "Get property from a map, if the property is a string matching a key
+  in the symbol table, return the value of that key instead. If the property
+  is a map with a single 'id' property, return either the value of the property
+  or the value of the referent of that property, if it exists in the symbol table"
+  [t loc prop]
+  (cond 
+    (fn? prop) (prop loc)
+    (instance? List loc) (map #(ld-get t % prop) loc)
+    :else (let [v (get loc prop)]
+      (cond 
+        (string? v) (resolve-id t v)
+        (instance? Map v) (if-let [vn (get v "id")] (resolve-id t vn) v)
+        (instance? List v) (map #(resolve-id t %) v)
+        :else v))))
+
+(defn ld->
+  "Take a symbol table, a location, and a set of predicates, 
+  return the set of nodes mapping to a given property"
+  [t loc & preds]
+  (let [res (reduce #(ld-get t %1 %2) loc preds)]
+    ;(println preds res)
+    res))
+
+(defn ld1->
+  "Take the first result of a ld-> path expression"
+  [t loc & preds]
+  (first (ld-> t loc preds)))
+
+(defn prop=
+  "Filter ressults where a specific key (or recursive chain of keys}
+  is equal to value"
+  [t v & ks]
+  (fn [m] (if (instance? List m)
+            (filter (fn [m1] (let [r (apply ld-> t m1 ks)]
+                               (if (instance? List r) (some #(= % v) r) (= r v))))  m)
+            (when (= v (ld-> t m ks)) m))))
+
+(defn convert-jsonld
+  "Convert java structures from jsonld library into clojure structures"
+  [m]
+  (walk/postwalk (fn [n] (cond
+                           (instance? Map n) (into {} n)
+                           (instance? List n) (into [] n)
+                           :else n)) m))
+
+(defn construct-symbol-table
+  [coll]
+  (let [graph (into [] (get coll "@graph"))]
+    (reduce (fn [acc v] (assoc acc (.get v "id") (convert-jsonld v))) {} graph)))
+
+(defn flatten-interpretation
+  [interp-path context-path]
+  (with-open [ir (io/reader interp-path)
+              cxr (io/reader context-path)]
+    (let [i (json/parse-stream ir)
+          cx (json/parse-stream cxr)
+          opts (JsonLdOptions.)]
+      (-> (JsonLdProcessor/flatten i cx opts) convert-jsonld))))
+
+(defn read-ld
+  [interp-path context-path]
+  (construct-symbol-table (flatten-interpretation interp-path context-path)))
+
+
+
