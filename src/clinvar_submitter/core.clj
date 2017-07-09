418adb822196aee6aab3a27bb0a35053dcfe9654
Tristan Nelson <tristan@iseult.us>
2017-07-09 07:04:59 -0400

    Starting testing, writing output to CSV file

--- src/clinvar_submitter/core.clj
+++ src/clinvar_submitter/core.clj
@@ -1,26 +1,28 @@
 (ns clinvar-submitter.core
-  (:require [clinvar-submitter.ld :as ld :refer [ld-> ld1-> prop=]])
+  (:require [clinvar-submitter.ld :as ld :refer [ld-> ld1-> prop=]]
+            [clojure-csv.core :as csv])
   (:gen-class))
 
 (defn construct-variant
   "Construct and return one row of variant table, with VariantInterpretation as root"
   [t i]
-  (let [cx-allele (ld1-> t i "cg:A122" "cg:relatedContextualAllele" (prop= t true "cg:preferred" ))]
+  (let [cx-allele (ld1-> t i "variant" "relatedContextualAllele" (prop= t true "preferred" ))]
     ["" ; local id
      "" ; linking id
      "" ; gene symbol
-     (ld-> t cx-allele "cg:referenceCoordinate" "cg:A052" "cg:A064")]))
+     (ld-> t cx-allele "referenceCoordinate" "referenceSequence" "display")]))
 
 (defn construct-variant-table
   "Construct and return variant table"
   [interp-path context-path]
   (let [t (ld/read-ld interp-path context-path)
         m (vals t)
-        interps ((prop= t "cg:VariantInterpretation" "@type") m)
+        interps ((prop= t "VariantInterpretation" "type") m)
         rows (map #(construct-variant t %) interps)]
     rows))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "take input assertion, transformation context, and output filename as input
+  and write variant table in csv format"
+  [in cx out & args]
+  (spit out (csv/write-csv (construct-variant-table in cx))))
--- test/clinvar_submitter/core_test.clj
+++ test/clinvar_submitter/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [clinvar-submitter.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-variant-table
+  (testing "Test correctness of variant table construction"
+    (is (= '(["" "" "" "NC_000012.12"])
+           (construct-variant-table "data/test_interp_1.dmwg.json" "data/context-new.jsonld")))))
+
+;; (deftest a-test
+;;   (testing "FIXME, I fail."
+;;     (is (= 0 1))))
