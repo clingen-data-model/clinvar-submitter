eaf492ce26a1646cb51d5713f7ff19abbbf97d66
Larry Babb <larry.babb@geneinsight.com>
2017-07-24 18:14:20 -0400

    added more column mappings, started evidence and citation aggregation work for explanation and pubmed id collection of met evidence.

--- src/clinvar_submitter/core.clj
+++ src/clinvar_submitter/core.clj
@@ -4,132 +4,62 @@
             [clojure-csv.core :as csv])
   (:gen-class))
 
-; TODO build out formal condition-XXX methods with clinvar rules and logging.
-;((defn condition-name
-;  "Returns the condition name based on the clinvar submission rules.
-;   The disease (dis) and phenotype (phen) maps are passed in as well in order
-;   to determine whether any warnings should be reported. 
-;   Additionally, the significance (sig) of the interp is passed in to
-;   determine if the [Not Specified] name should be defaulted for B, LB, VUS."
-;  [c dis phen sig]
-;  (let [cn (if (nil? c) nil (get c "name"))]
-;    ; test to see if no name and no diseases or phenotypes were provided. if so, report warning.
-;    (if (and (every? nil? '(c cn dis phen)) (some (partial = (lower cn)) ["not specified" "not provided"] (lower cn) )) 
-;      (log "WARNING - No condition name, disease or phenotype has been specified for a Path or Likely Path interpretation.")
-;      ())))
-      
-
-(defn condition-name
-  "Returns clinvar condition name based on available content. 
-  May log warnings if available content does not conform to
-  clinvar specifcations."
-  [c]
-  (get c "name"))
-
-(defn condition-idtype
-  ;TODO modify to deal with phenotypes and multi-values for satisfying clinvar specs.
-  [t c]
-  (let [disease-coding (ld-> t c "disease" "coding")]
-    (let [disease-code (get disease-coding "code")]
-      (get (re-find #"(.*)\_(.*)" disease-code) 1))))
-
-(defn condition-idvals
-   ;TODO modify to deal with phenotypes and multi-values for satisfying clinvar specs.
-  [t c]
-  (let [disease-coding (ld-> t c "disease" "coding")]
-    (let [disease-code (get disease-coding "code")]
-      (get (re-find #"(.*)\_(.*)" disease-code) 2))))
-
-(defn condition-moi
-  [t c]
-  [""])
-
-(defn get-condition
-  "Processes the condition element to derive the ClinVar sanctioned fields: 
-   for idtype & idvalue or preferred name. 
-
-   -- conflicting disease, phenotype and/or name values
-   If more than one of disease, phenotype or name is not nil than a warning should 
-   be reported 'disease and phenotypes should not be combined in clinvar 
-   submission form' or 'preferred name and disease/phenotype should not be 
-   combined in clinvar submission form'.
-   
-   -- multiple disease.codings or phenotype.codings with different idtypes
-   Also, if more than one coding is provided and they do not all have the same
-   idtype a warning should be reported 'clinvar requires a single idtype, only XXX 
-   is being included in the submission.'  
-
-   If more than one coding exists with a single idtype, then all idvalues should be 
-   separated with semi-colons.
-   
-   -- blank or nil disease, phenotype or name values
-   if the disease, phenotype and name values are all blank (or nil) and the 
-   clinical significance value is not Path or Lik Path, then the name will
-   be set to 'Not Specified' by default.  If it is Path or Lik Path a
-   warning will be reported 'Condition is missing for a clinically significant
-   interpretation.'"
-  [t i]
-  (let [c (ld-> t i "condition")]
-     {:name (condition-name c),
-      :idtype (condition-idtype t c),
-      :idvalue (condition-idvals t c),
-      :moi (condition-moi t c)}))
-
-(defn get-cx-allele
-  "Returns a map of all variant related fields needed for the clinvar submission
-   form.
-     :variantId - canonical allele with ClinGen AR PURL,
-     :refseq - preferred accession build38 ie. NC_000014.9
-     :start - genomic start pos (1-based)
-     :end - genomic end pos (1-based)
-     :ref - ref seq
-     :alt - alternate seq"
-  ; TODO finish building map here and using as described above.
-  [t i]
-  (ld1-> t i "variant" "relatedContextualAllele" (prop= t true "preferred")))
-
 (defn construct-variant
   "Construct and return one row of variant table, with VariantInterpretation as root"
   [t i]
-  (let [cx-allele (get-cx-allele t i)
-        condition (get-condition t i)]
-    ["" ; local id  -- TODO need to figure out if there's a stable id we can depend on here with VCI team.
-     "" ; linking id  
-     "" ; gene symbol
-     (form/csv-colval (ld-> t cx-allele "referenceCoordinate" "referenceSequence" "display")) ;rdefseq
-     "" ; hgvs
-     "" ; chromosome
-     (form/csv-colval (ld-> t cx-allele "referenceCoordinate" "start" "index")) ; start
-     (form/csv-colval (ld-> t cx-allele "referenceCoordinate" "end" "index")) ; stop
-     (form/csv-colval (ld-> t cx-allele "referenceCoordinate" "refAllele")) ; ref
-     (form/csv-colval (ld-> t cx-allele "allele")) ; alt
-     "" ; variant type
-     "" ; outer start
-     "" ; inner start
-     "" ; inner stop
-     "" ; outer stop
-     "" ; variant length
-     "" ; copy number
-     "" ; ref copy number
-     "" ; breakpoint 1
-     "" ; breakpoint 2
-     "" ; Trace or probe data	
+  (let [variant (form/get-variant t i)
+        condition (form/get-condition t i)
+        interp (form/get-interpretation t i)
+        ]
+    [(get interp :id) ; local id
+     "" ; linking id - only needed if providing case data or func evidence tabs 
+     "" ; gene symbol - not provided since we are avoiding contextual allele representation. 
+     (get variant :refseq) ;rdefseq
+     "" ; hgvs - not providing since we are avoiding contextual allele representation
+     "" ; chromosome - not providing since we are using the refseq field to denote the accession.
+     (get variant :start) ; start + 1  (from 0-based to 1-based)
+     (get variant :stop)  ; stop + 1  (from 0-based to 1-based)
+     (get variant :ref)   ; ref
+     (get variant :alt)   ; alt
+     "" ; variant type - non sequence var only
+     "" ; outer start - non sequence var only
+     "" ; inner start - non sequence var only
+     "" ; inner stop - non sequence var only
+     "" ; outer stop - non sequence var only
+     "" ; variant length - non sequence var only
+     "" ; copy number - non sequence var only
+     "" ; ref copy number - non sequence var only
+     "" ; breakpoint 1 - non sequence var only
+     "" ; breakpoint 2 - non sequence var only
+     "" ; Trace or probe data	 - non sequence var only
 		 "" ; empty
-		 "" ; Variation identifiers	(http://reg.genome.network.org/allele = ABC ABC:CA123123123)
-		 "" ; Location	
-		 "" ; Alternate designations 	
-		 "" ; Official allele name	
-		 "" ; URL	
+		 (get variant :variantIdentifier) ; Variation identifiers	(http://reg.genome.network.org/allele = ABC ABC:CA123123123)
+		 "" ; Location	- N/A
+		 "" ; Alternate designations - N/A 	
+		 "" ; Official allele name	- N/A
+		 "" ; URL	- bypassing for now, no set home for a public URL at this time
 		 "" ; empty 
-     (form/csv-colval (get condition :idtype)) ; Condition ID type (PURL)- assumes variantInterpretation.condition.disease.coding[0].code match of everything before the underscore.
-		 (form/csv-colval (get condition :idvalue))  ; Condition ID value	- assumes variantInterpretation.condition.disease.coding[0].code match of everything after the underscore.
-		 (form/csv-colval (get condition :name)) ; Preferred condition name
+     (get condition :idtype) ; Condition ID type (PURL)- assumes variantInterpretation.condition.disease.coding[0].code match of everything before the underscore.
+		 (get condition :idvalue)  ; Condition ID value	- assumes variantInterpretation.condition.disease.coding[0].code match of everything after the underscore.
+		 (get condition :name) ; Preferred condition name
      "" ; Condition category	
      "" ; Condition uncertainty	
      "" ; Condition comment	
      "" ; empty	
-     (form/csv-colval (ld-> t i "clinicalSignificance" "display")) ; Clinical significance	
-     "" ; Date last evaluated
+     (get interp :significance) ; Clinical significance	
+     (get interp :eval-date) ; Date last evaluated
+     "" ; assertion method
+     "" ; assertion method citations
+     (get condition :moi) ; Mode of Inheritance
+     ;(get evidence :pmid-citations) ; significance citations
+     "" ; significance citations without db ids
+     (form/get-met-evidence t i) ; comment on clinical significance
+     "" ; explanation if clinsig is other or drug
+     "" ; drug response condition
+     "" ; func consequence
+     "" ; comment on func consequence
+     "" ; empty
+     "" ; collection method
      "" ; TODO add remaining values from Variant Sheet in CLINVAR submission form...
      ]))
 
--- src/clinvar_submitter/form.clj
+++ src/clinvar_submitter/form.clj
@@ -7,3 +7,172 @@
 	 [v]
 	 (if (nil? v) "" (if (number? v) (str v) v)))
  
+ ; *** Interpretation related transformations
+ (defn interp-id
+   "Return the id portion of the @id url by taking the last part 
+    of the url for VariantInterpretation"
+   [i]
+   (let [full-id (get i "id")]
+     (csv-colval (get (re-find #"\/([a-z0-9\-]+)\/$" full-id) 1))))
+ 
+  (defn interp-significance
+  "Return the interpretation clinical significance."
+  [t i]
+  (let [significance (ld-> t i "clinicalSignificance")]
+    (csv-colval (get significance "display"))))
+  
+  (defn interp-eval-date
+  "Return the interpretation evaluation date."
+  [t i]
+  (let [contribution (ld-> t i "contribution")]
+    (if (nil? (get contribution "onDate")) "" 
+      (.format 
+        (java.text.SimpleDateFormat. "yyyy-MM-dd") 
+        (.parse
+          (java.text.SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX")
+          (get contribution "onDate"))))))
+  
+ (defn get-interpretation
+   "Returns a map of all interpretation related fields needed for the 
+    clinvar 'variant' submission sheet."
+   [t i]
+   {:id (interp-id i),
+    :significance (interp-significance t i),
+    :eval-date (interp-eval-date t i)})
+ 
+ ; *** Variant related transformations
+ (defn variant-identifier
+  "Return variant identifier, typically the ClinGen AlleleReg id."
+  [v]
+  (csv-colval (get v "CanonicalAllele")))
+ 
+ (defn variant-refseq
+  "Return the variant reference sequence."
+  [t v]
+  (let [refseq (ld-> t v "referenceCoordinate" "referenceSequence")]
+    (csv-colval (get refseq "display"))))
+ 
+ (defn variant-start
+  "Return the variant reference sequence start pos (1-based transform)."
+  [t v]
+  ; TODO figure out how to go from 0-based to 1-based
+  (let [start (ld-> t v "referenceCoordinate" "start")]
+    (csv-colval (get start "index"))))
+ 
+ (defn variant-stop
+  "Return the variant reference sequence stop pos (1-based transform)."
+  [t v]
+  ; TODO figure out how to go from 0-based to 1-based
+  (let [stop (ld-> t v "referenceCoordinate" "end")]
+    (csv-colval (get stop "index"))))
+ 
+ (defn variant-ref
+  "Return the variant ref allele sequence."
+  [t v]
+  (let [refseq (ld-> t v "referenceCoordinate")]
+    (csv-colval (get refseq "refAllele"))))
+ 
+ (defn variant-alt
+   "Return the variant alt allele sequence."
+   [v]
+   (csv-colval (get v "allele")))
+ 
+ (defn get-variant
+  "Returns a map of all variant related fields needed for the clinvar 
+   'variant' submission form.
+     :variantId - canonical allele with ClinGen AR PURL,
+     :refseq - preferred accession build38 ie. NC_000014.9
+     :start - genomic start pos (transform to 1-based)
+     :stop - genomic end pos (transform to 1-based)
+     :ref - ref seq
+     :alt - alternate seq"
+  [t i]
+  (let [v (ld1-> t i "variant" "relatedContextualAllele" (prop= t true "preferred"))]
+    {:variantIdentifier (variant-identifier v),
+     :refseq (variant-refseq t v),
+     :start (variant-start t v),
+     :stop  (variant-stop t v),
+     :ref (variant-ref t v),
+     :alt (variant-alt v)}))
+ 
+    ; *** Condition related transformations
+ 
+    ; TODO build out formal condition-XXX methods with clinvar rules and logging.
+;((defn condition-name
+;  "Returns the condition name based on the clinvar submission rules.
+;   The disease (dis) and phenotype (phen) maps are passed in as well in order
+;   to determine whether any warnings should be reported. 
+;   Additionally, the significance (sig) of the interp is passed in to
+;   determine if the [Not Specified] name should be defaulted for B, LB, VUS."
+;  [c dis phen sig]
+;  (let [cn (if (nil? c) nil (get c "name"))]
+;    ; test to see if no name and no diseases or phenotypes were provided. if so, report warning.
+;    (if (and (every? nil? '(c cn dis phen)) (some (partial = (lower cn)) ["not specified" "not provided"] (lower cn) )) 
+;      (log "WARNING - No condition name, disease or phenotype has been specified for a Path or Likely Path interpretation.")
+;      ())))
+ 
+ (defn condition-name
+  "Returns clinvar condition name based on available content. 
+  May log warnings if available content does not conform to
+  clinvar specifcations."
+  [t c]
+  (let [name (get c "name")]
+    (csv-colval (if (nil? name) "" name))))
+
+(defn condition-idtype
+  ;TODO modify to deal with phenotypes and multi-values for satisfying clinvar specs.
+  [t c]
+  (let [disease-coding (ld-> t c "disease" "coding")]
+    (let [disease-code (get disease-coding "code")]
+      (csv-colval (get (re-find #"(.*)\_(.*)" disease-code) 1)))))
+
+(defn condition-idvals
+   ;TODO modify to deal with phenotypes and multi-values for satisfying clinvar specs.
+  [t c]
+  (let [disease-coding (ld-> t c "disease" "coding")]
+    (let [disease-code (get disease-coding "code")]
+      (csv-colval (get (re-find #"(.*)\_(.*)" disease-code) 2)))))
+
+(defn condition-moi
+  [t c]
+  (csv-colval (get c "modeOfInheritance")))
+
+(defn get-condition
+  "Processes the condition element to derive the ClinVar sanctioned fields: 
+   for idtype & idvalue or preferred name. 
+
+   -- conflicting disease, phenotype and/or name values
+   If more than one of disease, phenotype or name is not nil than a warning should 
+   be reported 'disease and phenotypes should not be combined in clinvar 
+   submission form' or 'preferred name and disease/phenotype should not be 
+   combined in clinvar submission form'.
+   
+   -- multiple disease.codings or phenotype.codings with different idtypes
+   Also, if more than one coding is provided and they do not all have the same
+   idtype a warning should be reported 'clinvar requires a single idtype, only XXX 
+   is being included in the submission.'  
+
+   If more than one coding exists with a single idtype, then all idvalues should be 
+   separated with semi-colons.
+   
+   -- blank or nil disease, phenotype or name values
+   if the disease, phenotype and name values are all blank (or nil) and the 
+   clinical significance value is not Path or Lik Path, then the name will
+   be set to 'Not Specified' by default.  If it is Path or Lik Path a
+   warning will be reported 'Condition is missing for a clinically significant
+   interpretation.'"
+  [t i]
+  (let [c (ld-> t i "condition")]
+     {:name (condition-name t c),
+      :idtype (condition-idtype t c),
+      :idvalue (condition-idvals t c),
+      :moi (condition-moi t c)}))
+
+  (defn get-met-evidence
+   "Returns a collated map of all 'met' evidence records needed for the 
+    clinvar 'variant' submission sheet."
+   [t i]
+   (let [e (ld-> t i "evidence"  (prop= t "met" "information" "outcome" "code"))]
+     (let [crits (ld-> t e "information" "criterion")]
+       (clojure.string/join ", " (map #(get % "id") crits)))))
+ 
