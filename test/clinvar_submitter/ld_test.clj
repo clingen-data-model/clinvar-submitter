2faabcfd812fcd88e3d79d11a4acc1be60fbbbd2
Nafisa Khandaker <nkhandaker@partners.org>
2017-07-25 12:29:05 -0400

    test case for ns clinvar-submitter.ld

--- test/clinvar_submitter/ld_test.clj
+++ test/clinvar_submitter/ld_test.clj
@@ -2,16 +2,16 @@
   (:require [clojure.test :refer :all]
             [clinvar-submitter.ld :refer :all]))
 
-(deftest read-ld-test
+(deftest resolve-id-test
   (testing "Test if an identifier is in symbol table"
            (let [t (read-ld "data/test_interp_1.dmwg.json" "data/context-new.jsonld")]
-           (is (= "http://reg.genome.network/allele/CA229854"
-           (resolve-id t "http://reg.genome.network/allele/CA229854")
-                   (println " Identifier is in symbol table"))))))
+           (is (= {"id" "https://www.ncbi.nlm.nih.gov/pubmed/12501224", "type" "InformationSource"}
+                  (resolve-id t "https://www.ncbi.nlm.nih.gov/pubmed/12501224"))
+                  (println " Identifier is in symbol table")))))
 
 (deftest ld-get-test
   (testing "Test if an map key value is in symbol table"
   (let [t (read-ld "data/test_interp_1.dmwg.json" "data/context-new.jsonld")]
-  (is (= t
-         (ld-get t "cg:A156" "cg:A158")
-         (println " Map is in symbol table"))))))
+  (is (= nil
+         (ld-get t "id" "https://www.ncbi.nlm.nih.gov/pubmed/12501224"))
+         (println " Map is in symbol table")))))
