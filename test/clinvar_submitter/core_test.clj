29643a163d710f0caf69723fbfcdbbcd85e2ac99
Nafisa Khandaker <nkhandaker@partners.org>
2017-08-09 13:08:03 -0400

    Test cases added

--- test/clinvar_submitter/core_test.clj
+++ test/clinvar_submitter/core_test.clj
@@ -1,20 +1,26 @@
 (ns clinvar-submitter.core-test
   (:require [clojure.test :refer :all]
-            [clinvar-submitter.core :refer :all]))
+            [clinvar-submitter.core :refer :all]
+            [clinvar-submitter.ld :as ld]
+            [clinvar-submitter.form :as form]))
+
+(def interp-path "data/dmwg2.json")
+(def context-path "data/context-new.jsonld")
+(def t (ld/generate-symbol-table interp-path context-path))
+(def m (vals t))
+(def interps ((ld/prop= t "VariantInterpretation" "type") m))
+(def i (first interps))
+(def variant (form/get-variant t i))
+(def expectedvalue (construct-variant t i))
 
 (deftest test-variant-table
   (testing "Test correctness of variant table construction"
-    (is (= '(["" "" "" "NC_000012.12" "" "" ""])
-           (construct-variant-table "data/test_interp_1.dmwg.json" "data/context-new.jsonld")))))
+    (is (= (map #(construct-variant t %) interps) (construct-variant-table interp-path context-path))
+    (println "Expectedvalue" (map #(construct-variant t %) interps)))))
 
-;(deftest test-xyz
-;  (testing "this is the description of what is being tested"
-;		 (is (= '([])
-;		        (functiontotest arg1 arg2 ...)))))
+(deftest test-variant-table
+  (testing "Testing correctness ovariant construction"
+    (is (= expectedvalue (construct-variant t i))
+    (println "Expectedvalue" expectedvalue))))
 
-  
-  
-  
-;; (deftest a-test
-;;   (testing "FIXME, I fail."
-;;     (is (= 0 1))))
+(run-tests)
