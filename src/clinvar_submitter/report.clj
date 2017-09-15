9dec1d6b58e5f47805a382474d4bb242819f91a7
Nafisa Khandaker <nkhandaker@partners.org>
2017-09-15 14:39:42 -0400

    Added table header (Need formating)

--- src/clinvar_submitter/report.clj
+++ src/clinvar_submitter/report.clj
@@ -1,24 +1,27 @@
 (ns clinvar-submitter.report
-  (:require [clojure.string :as str]         
+  (:require [clojure.string :as str]
+            [clojure.pprint :as pprint]
+            [clojure.java.io :as io] 
   (:gen-class)))
 
 (defn report-header [in cx out frc r]
-  (->> ["ClinVar-Submitter Run Report"
-       "\n\nDate/Time: "(new java.util.Date)
-       "\nFile(s): " in
-       "\nJSON-LD Context (-c): " cx 
-       "\nOutput File (-o): " out
-       "\nForce overwrite (-f): " frc
-       "\nRun Report File (-r): " r 	"<run-report-filename (def:clinvar-submitter-run-report.txt>"
-       "\n\nNOTE: The variant coordinates are based on the preferred genomic representation from the ClinGen Allele Registry."
-       "\n========================================"
-       "\n\nFilename: " in
-       "\nRecord#: 	1"]
-       (str/join)))
+  (->> ["\nClinVar-Submitter Run Report"
+       "Date/Time: "(new java.util.Date)
+       "File(s): " in
+       "JSON-LD Context (-c): " cx 
+       "Output File (-o): " out
+       "Force overwrite (-f): " frc
+       "Run Report File (-r): " r 	"<run-report-filename (def:clinvar-submitter-run-report.txt>"
+       "NOTE: The variant coordinates are based on the preferred genomic representation from the ClinGen Allele Registry.\n\n"]      
+       (str/join \newline)))
+
+(def table-header {:Result# "101" :Input "File Name | Record | Variant (alt desig)" :Output "Cell | Status | Code | Description"})
 
 (defn write-report [in cx out frc reportfile]
-  (spit reportfile (report-header in cx out frc reportfile) :append false))
-
+  (spit reportfile (report-header in cx out frc reportfile) :append false)
+  ;writing table header. TODO - reformat  
+  (with-open [report (clojure.java.io/writer  reportfile :append true)]
+  (pprint/pprint table-header report)))
+  
 (defn append-to-report [reportfile errormsg]
-  (println reportfile errormsg)
   (spit reportfile errormsg :append true))
